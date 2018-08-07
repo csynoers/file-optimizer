d2af3135d3c1bf8b3f561263f06523c6609ac1c4
SCM <36254187+csynoers@users.noreply.github.com>
2018-08-07 10:59:18 +0700

    Update upload_helper.php

--- upload_helper.php
+++ upload_helper.php
@@ -1,91 +1,91 @@
 <?php
-/*
-MIT License
+    /*
+    MIT License
 
-Copyright (c) 2018 SCM
+    Copyright (c) 2018 SCM
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-function img_resize($files,$maxDim,$path_destination){
-    // $maxDim = 800;
-    $tmp_name 	= $files['tmp_name'];
-    $info 		= getimagesize($tmp_name);
-    $mime 		= $info['mime'];
-    
-    switch ($mime) {
-        case 'image/jpeg':
-            $image_create_func 	= 'imagecreatefromjpeg';
-            $image_create 		= 'imagecreate';
-            $image_save_func 	= 'imagejpeg';
-            $new_image_ext 		= 'jpg';
-            break;
-
-        case 'image/png':
-            $image_create_func 	= 'imagecreatefrompng';
-            $image_create 		= 'imagecreate';
-            $image_save_func 	= 'imagepng';
-            $new_image_ext 		= 'png';
-            break;
-
-        case 'image/gif':
-            $image_create_func 	= 'imagecreatefromgif';
-            $image_create 		= 'imagecreate';
-            $image_save_func 	= 'imagegif';
-            $new_image_ext 		= 'gif';
-            break;
-
-        default: 
-        	throw new Exception('Unknown image type.');
-    }
-    
-    list($width, $height) 	= $info;
-    
-    if ( $width > $maxDim || $height > $maxDim ) {
-        $ratio 				= $width/$height;
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    */
+    function img_resize($files,$maxDim,$path_destination){
+        // $maxDim = 800;
+        $tmp_name   = $files['tmp_name'];
+        $info     = getimagesize($tmp_name);
+        $mime     = $info['mime'];
         
-        if( $ratio > 1) {
-            $new_width 		= $maxDim;
-            $new_height 	= $maxDim/$ratio;
+        switch ($mime) {
+            case 'image/jpeg':
+                $image_create_func  = 'imagecreatefromjpeg';
+                $image_create     = 'imagecreate';
+                $image_save_func  = 'imagejpeg';
+                $new_image_ext    = 'jpg';
+                break;
+
+            case 'image/png':
+                $image_create_func  = 'imagecreatefrompng';
+                $image_create     = 'imagecreate';
+                $image_save_func  = 'imagepng';
+                $new_image_ext    = 'png';
+                break;
+
+            case 'image/gif':
+                $image_create_func  = 'imagecreatefromgif';
+                $image_create     = 'imagecreate';
+                $image_save_func  = 'imagegif';
+                $new_image_ext    = 'gif';
+                break;
+
+            default: 
+              throw new Exception('Unknown image type.');
+        }
         
-        } else {
-            $new_width 		= $maxDim*$ratio;
-            $new_height 	= $maxDim;
+        list($width, $height)   = $info;
         
+        if ( $width > $maxDim || $height > $maxDim ) {
+            $ratio        = $width/$height;
+            
+            if( $ratio > 1) {
+                $new_width    = $maxDim;
+                $new_height   = $maxDim/$ratio;
+            
+            } else {
+                $new_width    = $maxDim*$ratio;
+                $new_height   = $maxDim;
+            
+            }
+
+            $src        = $image_create_func( $tmp_name );
+            $dst        = $image_create( $new_width, $new_height );
+            imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+            imagedestroy( $src );
+            $image_save_func( $dst, $tmp_name );
+            imagedestroy( $dst );
         }
 
-        $src 				= $image_create_func( $tmp_name );
-        $dst 				= $image_create( $new_width, $new_height );
-        imagecopyresampled( $dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-        imagedestroy( $src );
-        $image_save_func( $dst, $tmp_name );
-        imagedestroy( $dst );
+        $file_name        = $files['name'];
+      $temp           = explode( ".", $file_name );
+      $newfilename      = current( $temp ).'_'.round(microtime(true)) . '.' . end( $temp );
+      
+      if ( move_uploaded_file( $tmp_name , $path_destination. $newfilename ) ) {
+        return $newfilename;
+      }else{
+        return 'error';
+      };
+
     }
-
-    $file_name				= $files['name'];
-	$temp 					= explode( ".", $file_name );
-	$newfilename 			= current( $temp ).'_'.round(microtime(true)) . '.' . end( $temp );
-	
-	if ( move_uploaded_file( $tmp_name , $path_destination. $newfilename ) ) {
-		return $newfilename;
-	}else{
-		return 'error';
-	};
-
-}
 ?>
